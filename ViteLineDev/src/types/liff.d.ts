72f5c2b9e9104c4634319471cb17cc01f946b625
NattoMillin <96514631+NattoMillin@users.noreply.github.com>
2023-06-22 02:34:11 +0000

    LiffMockの型定義について

--- ViteLineDev/src/types/liff.d.ts
+++ ViteLineDev/src/types/liff.d.ts
@@ -1,6 +1,9 @@
-import { ExtendedInit,LiffMockApi } from '@line/liff-mock';
+import { ExtendedInit, LiffMockApi } from "@line/liff-mock";
 
-declare module '@line/liff' {
+declare module "@line/liff" {
+  interface Config {
+    mock?: boolean;
+  }
   interface Liff {
     init: ExtendedInit;
     $mock: LiffMockApi;
