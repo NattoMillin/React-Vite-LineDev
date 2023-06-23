d34e8f518499820154b706176c8b5bdb948278cd
Natto Millin <zz112ggww@icloud.com>
2023-06-23 02:55:21 +0000

    commit build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({    
-  base: process.env.GITHUB_PAGES  // この行を追加
+  base: process.env.Vite_GITHUB_PAGES  // この行を追加
       ? "React-Vite-LineDev"            // この行を追加
       : "./",                     // この行を追加
   plugins: [react()],
