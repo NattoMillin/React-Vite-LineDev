db56ffd1ac500515847fb6542a7b50af1286ddf9
Natto Millin <zz112ggww@icloud.com>
2023-06-23 00:46:07 +0000

    Send Message Function create

--- ViteLineDev/src/components/Home.tsx
+++ ViteLineDev/src/components/Home.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form';
 import { Input } from './FormItems/Input';
 import { Options } from './FormItems/Select';
+import { RtnMsg, getUserInfo } from './Msg/Message';
+
 
 export type FormValues = {
     FirstName: string;
@@ -17,11 +19,15 @@ function Home() {
     const items:string[] =["female","male"]
     const onSubmit = (data: FormValues) => console.log(data);
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Input control={control} name="FirstName" errors={errors} rules={{ required: true }} />
-            <Options Items={items} errors={errors} control={control} name="SelectItem" rules={{ required: true }} />
-            <input type="submit" />
-        </form>
-    );
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input control={control} name="FirstName" errors={errors} rules={{ required: true }} />
+                <Options Items={items} errors={errors} control={control} name="SelectItem" rules={{ required: true }} />
+                <input type="submit" />
+            </form>
+            <button className="button" onClick={RtnMsg}>send message</button>
+            <button className="button" onClick={getUserInfo}>show user info</button>
+        </>
+);
 }
 export default Home;
