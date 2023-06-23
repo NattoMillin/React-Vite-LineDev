fe33dbb99739f61b0e4dc720c226b5b3ed62c6cf
Natto Millin <zz112ggww@icloud.com>
2023-06-23 01:01:47 +0000

    error fund page

--- ViteLineDev/vite.config.ts
+++ ViteLineDev/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({    
+  base: process.env.GITHUB_PAGES  // この行を追加
+      ? "React-Vite-LineDev"            // この行を追加
+      : "./",                     // この行を追加
   plugins: [react()],
 })
